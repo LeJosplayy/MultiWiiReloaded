4ed148e0c976bfb4a78c594c38739d353d7ed842
LeJosplayy <81078659+LeJosplayy@users.noreply.github.com>
2023-01-15 16:33:16 +0100

    Add files via upload

---
+++ MultiWiiReloaded/MultiWii.ino
@@ -0,0 +1,9 @@
+
+/*
+ * Welcome to MultiWii-Reloaded.
+ *
+ * If you see this message, chances are you are using the Arduino IDE. That is ok.
+ * To get the MultiWii program configured for your copter, you must switch to the tab named 'config.h'
+ *
+ */
+
